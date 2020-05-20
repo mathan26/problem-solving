73d41e3d0da9c759bf989ff75ef052b602d6ad3b
mathan26 <mathan.se@gmail.com>
2020-05-20 08:55:33 +0530

    Concepts: Collections with Java8: replaceAll() and sort() examples

--- problem-solving/src/com/github/mathan26/concepts/java8/Main.java
+++ problem-solving/src/com/github/mathan26/concepts/java8/Main.java
@@ -1,10 +1,9 @@
 package com.github.mathan26.concepts.java8;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
@@ -56,5 +55,66 @@ public class Main {
         //Lamda
         list.removeIf(s->s.length()>3);
         System.out.println(list);//[I, you, too]
+
+//        Requirement: Suppose there is a list of strings,
+//        all elements with a length greater than 3 are converted to uppercase, and the remaining elements remain unchanged.
+        list = new ArrayList<>(Arrays.asList("I", "Love", "you", "too"));
+
+
+        /*replaceAll()*/
+        //Java 7
+
+        for(int i=0;i<list.size();i++){
+            String str=list.get(i);
+            if((str.length()>3)){
+                list.set(i,str.toUpperCase());
+            }
+        }
+        System.out.println(list); //[I, LOVE, you, too]
+
+        //annonymous
+        list = new ArrayList<>(Arrays.asList("I", "Love", "you", "too"));
+        list.replaceAll(new UnaryOperator<String>() {
+            @Override
+            public String apply(String s) {
+                if(s.length()>3){
+                    return s.toUpperCase();
+                }
+                return s;
+            }
+        });
+        System.out.println(list);
+
+        //lamda
+        list = new ArrayList<>(Arrays.asList("I", "Love", "you", "too"));
+        list.replaceAll(s -> {
+            if(s.length()>3){
+                return s.toUpperCase();
+            }else{
+                return s;
+            }
+        });
+
+        System.out.println(list);
+
+        /*sort()*/
+
+//        Requirements: Suppose there is a list of strings,
+//        and the elements are sorted in ascending order according to the length of the string.
+        list = new ArrayList<>(Arrays.asList("I", "Love", "you", "too"));
+        Collections.sort(list,new Comparator<String>(){
+            @Override
+            public int compare(String str1, String str2) {
+                return str1.length()-str2.length();
+            }
+        });
+        System.out.println(list); //[I, you, too, Love]
+
+        list = new ArrayList<>(Arrays.asList("I", "Love", "you", "too"));
+
+//        lamda
+        list.sort((str1,str2)->str1.length()-str2.length());
+        System.out.println(list); //[I, you, too, Love]
+
     }
 }
