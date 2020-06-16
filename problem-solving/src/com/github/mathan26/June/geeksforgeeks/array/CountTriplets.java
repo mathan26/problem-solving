66285a7568ce8c5147a7a04f0e00fefc6c33f599
mathan26 <mathan.se@gmail.com>
2020-06-16 20:28:37 +0530

    Problem Solving

---
+++ problem-solving/src/com/github/mathan26/June/geeksforgeeks/array/CountTriplets.java
@@ -0,0 +1,46 @@
+package com.github.mathan26.June.geeksforgeeks.array;
+
+//Given an array of distinct integers. The task is to count all the triplets
+// such that sum of two elements equals the third element.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CountTriplets {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+
+        for(int i=0; i<t; i++){
+            int n=input.nextInt();
+            int[] arr = new int[n];
+            for(int k=0; k<n; k++){
+                arr[k] = input.nextInt();
+            }
+            findTriplets(n,arr);
+        }
+    }
+
+    private static void findTriplets(int n, int[] arr) {
+        Arrays.sort(arr);
+
+        int t=0;
+        for(int i=n-1; i>=0; i--){
+            int start=0;
+            int end=n-1;
+            while (start<end){
+                int sum=arr[start] + arr[end];
+                if(arr[i]==sum){
+                    t++;
+                    start++;
+                    end--;
+                }else if(arr[i]>sum){
+                    start++;
+                }else {
+                    end--;
+                }
+            }
+        }
+        System.out.println(t!=0?t:-1);
+    }
+}
---
+++ problem-solving/src/com/github/mathan26/June/geeksforgeeks/array/SubArraySolution.java
@@ -0,0 +1,57 @@
+package com.github.mathan26.June.geeksforgeeks.array;
+
+import java.util.Scanner;
+
+public class SubArraySolution {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int t=input.nextInt();
+
+        for(int i=0;i<t;i++){
+            int n=input.nextInt();
+            int sum=input.nextInt();
+
+            int[] arr=new int[n];
+
+            for (int k=0;k<n;k++){
+                arr[k] = input.nextInt();
+            }
+
+            findSubArray(n, sum, arr);
+        }
+    }
+
+    private static void findSubArray(int n, int sum, int[] arr) {
+
+        long result=arr[0];
+        int first=0;
+        int last=0;
+        while (result!=sum){
+            if(result>sum){
+                if(first==last){
+                    first++;
+                    last++;
+                    if(last>=n) {
+                        break;
+                    }
+                    result=arr[first];
+                }else {
+                    result -=arr[first];
+                    first++;
+                }
+            }else {
+                last++;
+                if(last>=n){
+                    break;
+                }else {
+                    result +=arr[last];
+                }
+            }
+        }
+        if(result!=sum){
+            System.out.println("-1");
+        }else {
+            System.out.println((first+1)+" "+(last+1));
+        }
+    }
+}
