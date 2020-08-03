91c7604efb5f645f242ea9487b426884575d8426
mathan26 <mathan.se@gmail.com>
2020-08-03 11:45:00 +0530

    leetcode: :zap: 155. Min Stack :zap:
    Approach:
    Create a own stack with val,min and next node.
    To Store the min compare with current element to head.min then assign to min
    When doing pop change the head to next.head

---
+++ problem-solving/src/com/github/mathan26/leetcode/challenges/april/MinStack.java
@@ -0,0 +1,51 @@
+package com.github.mathan26.leetcode.challenges.april;
+
+class MinStack {
+    private Node head;
+
+    public void push(int x) {
+        if(head == null)
+            head = new Node(x, x);
+        else
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(1);
+        minStack.push(2);
+        minStack.push(-1);
+        minStack.getMin();
+        minStack.pop();
+        minStack.top();
+        minStack.getMin();
+    }
+}
