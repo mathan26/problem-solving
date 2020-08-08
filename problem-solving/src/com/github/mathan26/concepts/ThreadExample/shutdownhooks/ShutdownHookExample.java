8c154d279b0d38d039d4a5853387a98cc9c94b83
mathan26 <mathan.se@gmail.com>
2020-08-08 12:54:40 +0530

    leetcode: :zap: 33. Search in Rotated Sorted Array           :zap:64. Minimum Path Sum
    Approach:
    33 - Binary Search , check range of the target assign the left and right
    64 - Fill the 0th row 0th column, then use the value update with dp array

---
+++ problem-solving/src/com/github/mathan26/concepts/ThreadExample/shutdownhooks/ShutdownHookExample.java
@@ -0,0 +1,27 @@
+package com.github.mathan26.concepts.ThreadExample.shutdownhooks;
+
+import org.omg.SendingContext.RunTime;
+
+public class ShutdownHookExample {
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new ThreadA());
+        System.out.println("Main Threading Running!");
+        // check the number of processors available
+        System.out.println("" + Runtime.getRuntime()
+                .availableProcessors());
+        // run the garbage collector
+        Runtime.getRuntime().gc();
+        // print the maximum memory
+        System.out.println("" + Runtime.getRuntime().maxMemory());
+        System.out.println("Gonna call System.exit()");
+        System.exit(0);
+    }
+}
+
+class ThreadA extends Thread{
+    public void run(){
+        System.out.println("########################");
+        System.out.println("addShutdownHook called! ");
+        System.out.println("Doing Clean up process!!!!");
+    }
+}
---
+++ problem-solving/src/com/github/mathan26/leetcode/challenges/april/MinimumSumPath.java
@@ -0,0 +1,32 @@
+package com.github.mathan26.leetcode.challenges.april;
+
+public class MinimumSumPath {
+    public int minPathSum(int[][] grid) {
+        int row = grid.length;
+        if(row==0)
+            return 0;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+
+        //assign the 0,0 to dp[0][0]
+
+        //Fill the 1st Row alone
+        dp[0][0] = grid[0][0];
+        for(int i=1;i<col;i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+
+        //Fill the 1st column
+        for(int i=1;i<row;i++){
+            dp[i][0] = dp[i -1][0] + grid[i][0];
+        }
+
+        //Fill the rest
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[row-1][col-1];
+    }
+}
