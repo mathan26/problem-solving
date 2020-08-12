c63409a4fedb3bf3feb46faf2f8b82de8c950d78
mathan26 <mathan.se@gmail.com>
2020-08-12 12:06:49 +0530

    leetcode: :zap: problems commit
    Approach:

---
+++ problem-solving/src/com/github/mathan26/leetcode/challenges/april/CnsrtBTreeFromPreOrder.java
@@ -0,0 +1,29 @@
+package com.github.mathan26.leetcode.challenges.april;
+
+
+public class CnsrtBTreeFromPreOrder {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return helper(preorder, 0, preorder.length - 1);
+    }
+
+    private TreeNode helper(int[] preorder, int start, int end) {
+        if(start > end) return null;
+
+        TreeNode node = new TreeNode(preorder[start]);
+        int i;
+        for(i=start;i<=end;i++) {
+            if(preorder[i] > node.val)
+                break;
+        }
+
+        node.left = helper(preorder, start+1, i-1);
+        node.right = helper(preorder, i, end);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        CnsrtBTreeFromPreOrder cnsrtBTreeFromPreOrder = new CnsrtBTreeFromPreOrder();
+        int[] preorder= {8,5,1,7,10,12};
+        cnsrtBTreeFromPreOrder.bstFromPreorder(preorder);
+    }
+}
---
+++ problem-solving/src/com/github/mathan26/leetcode/challenges/april/SubArraySumEqualsK.java
@@ -0,0 +1,84 @@
+package com.github.mathan26.leetcode.challenges.april;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubArraySumEqualsK {
+    public static void main(String[] args) {
+        int[] array={3,4,7,2,-3,1,4,2};
+        int k=7;
+        bruteForce(array,k);  // O(n2)
+        prefixSum(array,k);     // O(nlogn)
+        efficientSolutionUsingHashMap(array,k);
+
+
+    }
+
+    private static void efficientSolutionUsingHashMap(int[] array, int k) {
+        int counter=0;
+        int currentSum=0;
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+            currentSum +=array[i];
+
+            if(currentSum==k){
+                counter++;
+            }
+            if(map.containsKey(currentSum-k)){
+                counter = counter + map.get(currentSum-k);
+            }
+
+            map.put(currentSum,map.getOrDefault(currentSum,0)+1);
+
+        }
+
+        System.out.println(counter);
+    }
+
+    private static void prefixSum(int[] array, int k) {
+        int[] sumArray=new  int[array.length];
+        int sum=0;
+        int count=0;
+        for (int i = 0; i <array.length ; i++) {
+            sum +=array[i];
+            sumArray[i] =sum;
+        }
+        for (int i = 0; i < array.length; i++) {
+
+            if(sumArray[i]==k){
+                count++;
+            }
+            if(checkValueExists(sumArray[i]-k, sumArray)){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+
+    }
+
+    private static boolean checkValueExists(int value,int[] arr) {
+        for (int j = 0; j < arr.length; j++) {
+            if(value == arr[j]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void bruteForce(int[] array,int k) {
+        int counter=0;
+        for (int i = 0; i <array.length ; i++) {
+            int sum=0;
+            for (int j = i + 1; j <array.length ; j++) {
+                sum=sum + array[i] + array[j];
+                if(sum==k)
+                    counter++;
+            }
+        }
+
+        System.out.println(counter);
+    }
+}
